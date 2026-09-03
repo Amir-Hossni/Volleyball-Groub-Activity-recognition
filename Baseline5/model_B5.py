574fa89c5be2dfc03a38affe7f98bbf9df91907c
Amir Hosni <amiirhosnii548@gmail.com>
2026-09-03 20:27:20 +0300

    restrucher(b5_stage2)

--- Baseline5/model_B5.py
+++ Baseline5/model_B5.py
@@ -86,8 +86,7 @@ class GroupTemporalClassifierB5(nn.Module):
         person_model,
         num_classes=8,
         num_players=12,
-        player_feature_dim=512,
-        hidden_dim=2048,
+        hidden_dim=512,
         dropout=0.3,
     ):
         super().__init__()
@@ -95,14 +94,14 @@ class GroupTemporalClassifierB5(nn.Module):
         # Stage-A temporal person model (frozen)
         self.person_model = person_model
         self.num_players = num_players
-        self.player_feature_dim = player_feature_dim
+        self.player_feature_dim = person_model.lstm_hidden
 
         for param in self.person_model.parameters():
             param.requires_grad = False
         self.person_model.eval()
 
-        # Concatenation: (B, num_players, player_feature_dim) -> (B, input_dim)
-        input_dim = num_players * player_feature_dim
+        # Concatenation: MAX + MEAN
+        input_dim = (2 * self.player_feature_dim)
 
         # Stage-B classifier
         self.classifier = nn.Sequential(
@@ -111,10 +110,11 @@ class GroupTemporalClassifierB5(nn.Module):
             nn.LayerNorm(hidden_dim),
             nn.ReLU(inplace=True),
             nn.Dropout(dropout),
-            nn.Linear(hidden_dim, 1024),
-            nn.LayerNorm(1024),
+            nn.Linear(hidden_dim, hidden_dim // 2),
+            nn.LayerNorm(hidden_dim // 2),
             nn.ReLU(inplace=True),
-            nn.Linear(1024, num_classes),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim // 2, num_classes),
         )
 
     def train(self, mode=True):
@@ -132,18 +132,28 @@ class GroupTemporalClassifierB5(nn.Module):
 
         # Stage-A inference (frozen)
         with torch.no_grad():
-            player_features = self.person_model(x, return_features=True)
-            # (B*P, player_feature_dim)
+            player_features = self.person_model( x, return_features=True) # (B*P, 512)
+            
 
         # restore player dimension
-        player_features = player_features.reshape(B, P, self.player_feature_dim)
-        # (B, num_players, player_feature_dim)
+        player_features = player_features.reshape(B, P, self.player_feature_dim) # (B, P, 512)
+        
 
-        # concatenate all player representations
-        team_features = player_features.reshape(B, P * self.player_feature_dim)
-        # (B, num_players * player_feature_dim)
+        # MAX pooling across players
+        pooled_max = player_features.max(dim=1)[0] # (B, 512)
+        
 
-        output = self.classifier(team_features)  # (B, num_classes)
+        # MEAN pooling across players
+        pooled_mean = player_features.mean(dim=1) # (B, 512)
+        
+
+        # concatenate MAX + MEAN
+        team_features = torch.cat([pooled_max, pooled_mean], dim=-1) # (B, 1024)
+        
+
+        output = self.classifier(team_features) # (B, 8)
+        
+
         return output
 
 
@@ -220,27 +230,3 @@ class GroupTemporalClassifierB5V2(nn.Module):
 
     
     
-    
-# Best model saved (Val Accuracy = 76.53%)
-# Confusion matrix saved to /kaggle/working/confusion_matrix_val.png
-# Epoch 2/50, Train Loss: 0.4809 Train Accuracy: 83.68% Validation Loss: 0.7564 Validation Accuracy: 75.80% Validation F1: 0.4958
-# =========================
-# Epoch 3/50, Train Loss: 0.4276 Train Accuracy: 85.36% Validation Loss: 0.7776 Validation Accuracy: 75.15% Validation F1: 0.4936
-# =========================
-# Epoch 4/50, Train Loss: 0.3909 Train Accuracy: 86.36% Validation Loss: 0.8045 Validation Accuracy: 74.49% Validation F1: 0.4962
-# =========================
-# Epoch 5/50, Train Loss: 0.3594 Train Accuracy: 87.29% Validation Loss: 0.8310 Validation Accuracy: 73.93% Validation F1: 0.4990
-# =========================
-# Epoch 6/50, Train Loss: 0.3318 Train Accuracy: 88.22% Validation Loss: 0.8592 Validation Accuracy: 73.76% Validation F1: 0.4974
-# =========================
-# Epoch 7/50, Train Loss: 0.3070 Train Accuracy: 89.06% Validation Loss: 0.8823 Validation Accuracy: 73.52% Validation F1: 0.5019
-# =========================
-# Epoch 8/50, Train Loss: 0.2788 Train Accuracy: 90.12% Validation Loss: 0.9247 Validation Accuracy: 73.33% Validation F1: 0.4919
-# =========================
-# Epoch 9/50, Train Loss: 0.2523 Train Accuracy: 91.10% Validation Loss: 0.9600 Validation Accuracy: 73.02% Validation F1: 0.4957
-# =========================
-# Epoch 10/50, Train Loss: 0.2249 Train Accuracy: 92.21% Validation Loss: 1.0227 Validation Accuracy: 71.71% Validation F1: 0.4865
-# =========================
-# Epoch 11/50, Train Loss: 0.1966 Train Accuracy: 93.20% Validation Loss: 1.0940 Validation Accuracy: 71.34% Validation F1: 0.4885
-# =========================
-# Early stopping triggered    
